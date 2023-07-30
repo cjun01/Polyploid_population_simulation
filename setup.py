4b9cfd9b367d8561e67b7ee7cba8adeadd36649e
Zhe Cao <cjun01@gmail.com>
2023-07-30 15:55:26 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='population_simulation',
+    name='Polyploid_population_simulation',
     version='0.1',
     packages=find_packages(),
     install_requires=[
